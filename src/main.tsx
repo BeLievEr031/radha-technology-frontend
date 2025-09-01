661014599933a0db0790cbcd556fbd4515183baf
BeLieVer031 <sandyrajak031@gmail.com>
2025-09-01 18:07:46 +0530

    feat: add Root layout & routing.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1 className='bg-red-500'>Hello World!!</h1>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
