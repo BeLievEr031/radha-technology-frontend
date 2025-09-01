99de633a2a4fbbf100b55def8822102a97fea879
BeLieVer031 <sandyrajak031@gmail.com>
2025-09-02 00:12:27 +0530

    configured root layout for pages.

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "../components/Navbar"
 
 function Root() {
     return (
-        <main>
+        <main className="bg-primary-black min-h-screen text-white font-outfit">
+            <Navbar />
             <Outlet />
         </main>
     )
