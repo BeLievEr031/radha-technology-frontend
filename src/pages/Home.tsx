8115f72c09b00645a31d9eda3d7c5449940a7463
BeLieVer031 <sandyrajak031@gmail.com>
2025-09-02 17:28:15 +0530

    feat: add carousal section.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import About from "../sections/Home/About"
 import Hero from "../sections/Home/Hero"
+import Carousal from "../components/Carousal"
 
 function Home() {
     return (
         <React.Fragment>
             <Hero />
             <About />
+            <Carousal />
         </React.Fragment>
     )
 }
