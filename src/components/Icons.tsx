e3562e2025628e06cd1f68b84eb0bccee23ae8d4
BeLieVer031 <sandyrajak031@gmail.com>
2025-09-03 22:44:58 +0530

    feat: add footer section.

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -61,5 +61,10 @@ export function Check({ width = 40, height = 40 }: IProp) {
 
 }
 
+export function ArrowRight({ width = 40, height = 40 }: IProp) {
+    return <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-arrow-right-icon lucide-arrow-right"><path d="M5 12h14" /><path d="m12 5 7 7-7 7" /></svg>
+
+}
+
 
 
---
+++ src/sections/Home/Footer.tsx
@@ -0,0 +1,85 @@
+import { Link } from "react-router-dom"
+import CircleButton from "../../components/CircleButton"
+import { ArrowRight, Check } from "../../components/Icons"
+
+function Footer() {
+    return (
+        <footer className="px-30 py-20">
+            <h1 className="uppercase text-center text-primary text-[160px] font-semibold">Let’s Talk</h1>
+            <p className="text-[96px] font-semibold text-center cursor-pointer hover:text-primary transition-colors duration-300 ease-in-out">hello@radhatechnology.in</p>
+            <div className="flex justify-center pt-10">
+                <CircleButton>
+                    contact
+                </CircleButton>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-x-10 pt-20 items-start font-plus-jakarta">
+
+                {/* About Us */}
+                <div>
+                    <h3 className="text-white font-semibold text-[24px]">About Us</h3>
+                    <p className="leading-relaxed mt-2 text-primary-gray">
+                        At Radha Technology, we transform ideas into powerful websites and apps through innovation, creativity, and seamless collaboration.
+                    </p>
+                    {/* <div className="flex space-x-4 text-lg">
+                        <Link to="/" className="hover:text-primary"><FaFacebookF /></Link>
+                        <Link to="/" className="hover:text-primary"><FaXTwitter /></Link>
+                        <Link to="/" className="hover:text-primary"><FaInstagram /></Link>
+                        <Link to="/" className="hover:text-primary"><FaYoutube /></Link>
+                    </div> */}
+                </div>
+
+                {/* Navigation */}
+                <div>
+                    <h3 className="text-white font-semibold text-[24px]">Navigation</h3>
+                    <ul className="space-y-4 mt-2 text-[16px]">
+                        <li><Link to="/" className="hover:text-primary">Home</Link></li>
+                        <li><Link to="/" className="hover:text-primary">About</Link></li>
+                        <li><Link to="/" className="hover:text-primary">Services</Link></li>
+                        <li><Link to="/" className="hover:text-primary">My Projects</Link></li>
+                    </ul>
+                </div>
+
+                {/* Services */}
+                <div>
+                    <h3 className="text-white font-semibold text-[24px]">Services</h3>
+                    <ul className="space-y-4 mt-2">
+                        <li><Link to="/" className="hover:text-primary">Web Development</Link></li>
+                        <li><Link to="/" className="hover:text-primary">App Development</Link></li>
+                        <li><Link to="/" className="hover:text-primary">Wordpress Development</Link></li>
+                        <li><Link to="/" className="hover:text-primary">UX/UI Design</Link></li>
+                    </ul>
+                </div>
+
+                {/* Newsletter */}
+                <div>
+                    <h3 className="text-white font-semibold text-[24px]">Get a Call</h3>
+                    <form className="flex items-center border-b border-gray-600 focus-within:border-lime-400 pb-2 mt-2">
+                        <input
+                            type="text"
+                            placeholder="Enter your Phone Number"
+                            className="bg-transparent flex-1 p-2 outline-none text-sm text-gray-200 placeholder-gray-400"
+                        />
+                        <button
+                            type="submit"
+                            className="bg-primary flex justify-center items-center text-black p-2 rounded-full hover:bg-lime-300"
+                        >
+                            <ArrowRight width={25} height={25} />
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+            <div className="bg-secondary-black w-full mx-auto h-[1.5px] mt-10"></div>
+            <p className="font-plus-jakarta">© 2025 All rights for Radh Technology.</p>
+
+
+            <div className="text-[180px] uppercase font-bold pt-10 -space-y-18 tracking-wide">
+                <p>Radha</p>
+                <p>Technology</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
